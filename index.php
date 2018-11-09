f3f6da22628f125d713eef537556d8bb92adc7c6
Zack Maccario <Zack@Zacks-MacBook-Air.local>
2018-11-08 22:41:44 -0500

    Added some files from the example, contents=alldrinkers

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<title>Contents of Vending Machine</title>
+</head>
+
+<body>
+
+<center><h1>Contents of Vending Machine</h1></center>
+
+<p>
+<a href="contents.php">Contents of Vending Machine</a>
+</p>
+
+<p>
+The following shows many useful information about your PHP configuration.
+</p>
+<? phpinfo(); ?>
+
+</body>
+</html>
